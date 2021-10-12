c6a38d058f400de61f31e1705f34b5ed43db7aa9
sir306 <40708936+sir306@users.noreply.github.com>
2021-10-12 21:11:31 +1300

    update movingplatform

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform():
+	PlatformSpeed(10.0f)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+		float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+		if (JourneyTravelled >= JourneyLength)
+		{
+			FVector Swap = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = Swap;
+		}
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		Location += PlatformSpeed * DeltaTime * Direction;
+		SetActorLocation(Location);
+	}
+}
---
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+
+	/** Platform movement speed */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PlatformSpeed;
+
+	/** Target location to move to */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (MakeEditWidget = "true"))
+	FVector TargetLocation;
+
+
+private:
+
+	FVector GlobalTargetLocation;
+	FVector GlobalStartLocation;
+
+};
